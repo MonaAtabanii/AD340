86fa74834d1121bfc40dbdc6955f197734f23edb
MonaMohamed <Mona.Mohamed@seattlecentral.edu>
2021-05-04 23:50:37 -0700

    Startup assignment 4

---
+++ app/src/main/java/com/example/datingprofilew4/Profile.java
@@ -0,0 +1,24 @@
+package com.example.datingprofilew4;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Profile extends Fragment {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View v =  inflater.inflate(R.layout.fragment_profile, container, false);
+        return v;
+    }
+}
---
+++ app/src/main/java/com/example/datingprofilew4/SettingsFragment.java
@@ -0,0 +1,26 @@
+package com.example.datingprofilew4;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceFragmentCompat;
+
+public class SettingsFragment extends Fragment {
+
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View v =  inflater.inflate(R.layout.fragment_settings, container, false);
+        return v;
+    }
+}
